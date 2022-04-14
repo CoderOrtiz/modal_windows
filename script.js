96ee650947bd4970dd6f6fdcc8c3ef675ac97cf4
Daniel Ortiz <ortiz.daniel1994@gmail.com>
2022-04-14 15:04:31 -0400

    Added functions to manipulate CSS File

--- script.js
+++ script.js
@@ -4,9 +4,25 @@
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const closeModalBtn = document.querySelector(".close-modal");
-const showModalBtn = document.querySelectorAll(".show-modal");
+const openModalBtn = document.querySelectorAll(".show-modal");
+
+// Functions to Open and Close the Modal's
+function openModal(){
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+};
+
+function closeModal(){
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+};
 
 
 // Select All For Loop for "the showModalBtn" Variable
-for (let i = 0; i < showModalBtn.length; i++)
-console.log(showModalBtn[i].textContent);
+for (let i = 0; i < openModalBtn.length; i++)
+// When "Show Modal i" is clicked, removes the Hidden Classes
+openModalBtn[i].addEventListener("click", openModal);
+
+// When the X button is clicked, or User clicks outside Modal Window, adds the Hidden Class again
+closeModalBtn.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
