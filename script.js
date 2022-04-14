c78c9c7d804fd004bc93246ea7c29044bfd1deaa
Daniel Ortiz <ortiz.daniel1994@gmail.com>
2022-04-14 13:37:06 -0400

    Added to the JavaScript File

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+// Variables that Select Classes
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const closeModalBtn = document.querySelector(".close-modal");
+const showModalBtn = document.querySelectorAll(".show-modal");
+
+
+// Select All For Loop for "the showModalBtn" Variable
+for (let i = 0; i < showModalBtn.length; i++)
+console.log(showModalBtn[i].textContent);
